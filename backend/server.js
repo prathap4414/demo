b5e388f734a74489738ef05b751cc94288575460
Prathap M <www.prathu4413@gmail.com>
2026-01-08 19:55:56 +0530

    changes done to backned server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.get("/api/hello", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
